48aea3619bc7bdb2b68ac6bf73f5510b895cb38a
floweryclover <floweryclover@gmail.com>
2025-03-16 14:43:36 +0900

    코드추가: MainMenu, Lobby, Competitive 각각의 게임모드 상속 클래스 추가 및 레벨에 적용

---
+++ Source/ShootingStar/Private/CompetitiveGameMode.h
@@ -0,0 +1,17 @@
+// Copyright 2025 ShootingStar. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CompetitiveGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACompetitiveGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ShootingStar/Private/LobbyGameMode.h
@@ -0,0 +1,17 @@
+// Copyright 2025 ShootingStar. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALobbyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
