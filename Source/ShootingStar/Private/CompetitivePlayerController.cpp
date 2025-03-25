4c9c67e79f3cc798513546d38f7866ee9d2c555c
junghyun Lim <trgcame@naver.com>
2025-03-26 00:19:37 +0900

    기능추가: 테스트레벨에서 이동 및 사격 추가

--- Source/ShootingStar/Private/CompetitivePlayerController.cpp
+++ Source/ShootingStar/Private/CompetitivePlayerController.cpp
@@ -3,8 +3,101 @@
 
 #include "CompetitivePlayerController.h"
 #include "TeamComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "GameFramework/Pawn.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
+#include "ShootingStar/ShootingStarCharacter.h"
+#include "Engine/World.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
 
 ACompetitivePlayerController::ACompetitivePlayerController()
 {
 	TeamComponent = CreateDefaultSubobject<UTeamComponent>(TEXT("TeamComponent"));
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+}
+
+void ACompetitivePlayerController::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+}
+
+void ACompetitivePlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	LookMouse();
+}
+
+void ACompetitivePlayerController::SetupInputComponent()
+{
+	// set up gameplay key bindings
+	Super::SetupInputComponent();
+
+	// Add Input Mapping Context
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		// Setup Move input events
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ACompetitivePlayerController::Move);
+		EnhancedInputComponent->BindAction(ShootAction, ETriggerEvent::Started, this, &ACompetitivePlayerController::Shoot);
+
+	}
+}
+
+void ACompetitivePlayerController::Move(const FInputActionValue& Value)
+{
+
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	// find out which way is forward
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	// get forward vector
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+	// get right vector 
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	// add movement 
+	GetCharacter()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetCharacter()->AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void ACompetitivePlayerController::LookMouse()
+{
+	FHitResult Hit;
+	GetHitResultUnderCursor(ECC_Visibility, false, Hit);
+
+	if (Hit.bBlockingHit)
+	{
+		APawn* const MyPawn = GetPawn();
+		if (MyPawn)
+		{
+			FRotator LookRotation = UKismetMathLibrary::FindLookAtRotation(MyPawn->GetActorLocation(),
+				FVector(Hit.Location.X, Hit.Location.Y, MyPawn->GetActorLocation().Z));
+			MyPawn->SetActorRotation(LookRotation);
+		}
+	}
+
+}
+
+void ACompetitivePlayerController::Shoot()
+{
+	AShootingStarCharacter* ControlledCharacter = Cast<AShootingStarCharacter>(GetPawn());
+	if (ControlledCharacter)
+	{
+		ControlledCharacter->PullTrigger();
+	}
 }
--- Source/ShootingStar/ShootingStarCharacter.cpp
+++ Source/ShootingStar/ShootingStarCharacter.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ShootingStarCharacter.h"
-#include "Gun.h"
+#include "ShootingStar/Public/Gun.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Camera/CameraComponent.h"
 #include "Components/DecalComponent.h"
@@ -52,10 +52,15 @@ void AShootingStarCharacter::BeginPlay()
 
 	Health = MaxHealth;
 	
+	if (!GunClass)
+	{
+		UE_LOG(LogTemp, Error, TEXT("GunClass is nullptr! Did you forget to set it in the Blueprint?"));
+		return;
+	}
+
 	Gun = GetWorld()->SpawnActor<AGun>(GunClass);
 	if (Gun) 
 	{
-		Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, TEXT("WeaponSocket"));
 		Gun->SetOwner(this);
 	}
 }
@@ -77,6 +82,11 @@ void AShootingStarCharacter::Tick(float DeltaSeconds)
 }
 void AShootingStarCharacter::PullTrigger()
 {	
+	if (!Gun)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Gun is nullptr!"));
+		return;
+	}
 	Gun->PullTrigger();
 }
 float AShootingStarCharacter::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
@@ -85,6 +95,13 @@ float AShootingStarCharacter::TakeDamage(float DamageAmount, struct FDamageEvent
 	DamageToApply = FMath::Min(Health, DamageToApply);
 	Health -= DamageToApply;
 	UE_LOG(LogTemp, Warning, TEXT("Health left %f"), Health);
+
+	if (IsDead())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Character is dead!"));
+		// TODO: 죽었을 때 처리 (예: 애니메이션, 게임 오버, 리스폰 등)
+		Destroy(); // 캐릭터 삭제
+	}
 
 	return DamageToApply;
 }
