d192646013592b4370275f61141604eee3c65a24
floweryclover <floweryclover@gmail.com>
2025-03-25 20:31:07 +0900

    기능추가: Lobby Seamless travel 및 팀별 플레이어 목록 저장 구현

--- Source/ShootingStar/Private/LobbyGameMode.cpp
+++ Source/ShootingStar/Private/LobbyGameMode.cpp
@@ -2,9 +2,86 @@
 
 
 #include "LobbyGameMode.h"
+#include "CompetitiveSystemComponent.h"
 #include "LobbyPlayerController.h"
+#include "LobbyGameState.h"
 
 ALobbyGameMode::ALobbyGameMode()
+	: NumPlayers{1} // 항상 호스트 포함
 {
+	PrimaryActorTick.bCanEverTick = true;
+	bUseSeamlessTravel = true;
 	PlayerControllerClass = ALobbyPlayerController::StaticClass();
+	GameStateClass = ALobbyGameState::StaticClass();
+
+	CompetitiveSystemComponent = CreateDefaultSubobject<
+		UCompetitiveSystemComponent>(TEXT("CompetitiveSystemComponent"));
+}
+
+int32 ALobbyGameMode::GetNumPlayers()
+{
+	Super::GetNumPlayers();
+
+	return NumPlayers;
+}
+
+void ALobbyGameMode::Tick(const float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void ALobbyGameMode::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId,
+                              FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+	
+	if (NumPlayers >= CompetitiveSystemComponent->GetMaxPlayersPerTeam() * 2)
+	{
+		ErrorMessage = TEXT("정원이 가득 찼습니다.");
+		return;
+	}
+
+	NumPlayers += 1;
+}
+
+void ALobbyGameMode::PostLogin(APlayerController* const NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	const ETeam TeamToAssign = CompetitiveSystemComponent->GetTeamForNextPlayer(
+		GetGameState<AGameStateBase>()->PlayerArray);
+	if (TeamToAssign == ETeam::None)
+	{
+		// 플레이어 팀 할당 실패, 추방
+		Logout(NewPlayer);
+		return;
+	}
+
+	UTeamComponent* const TeamComponent = Cast<UTeamComponent>(
+		NewPlayer->GetComponentByClass(UTeamComponent::StaticClass()));
+	check(TeamComponent != nullptr);
+
+	TeamComponent->SetTeam(TeamToAssign);
+}
+
+void ALobbyGameMode::Logout(AController* const Exiting)
+{
+	Super::Logout(Exiting);
+	
+	NumPlayers -= 1;
+}
+
+void ALobbyGameMode::SwapPlayerControllers(APlayerController* OldPC, APlayerController* NewPC)
+{
+	Super::SwapPlayerControllers(OldPC, NewPC);
+
+	UTeamComponent* const OldTeamComponent = Cast<UTeamComponent>(
+		OldPC->GetComponentByClass(UTeamComponent::StaticClass()));
+	UTeamComponent* const NewTeamComponent = Cast<UTeamComponent>(
+		NewPC->GetComponentByClass(UTeamComponent::StaticClass()));
+	if (OldTeamComponent == nullptr || NewTeamComponent == nullptr)
+	{
+		return;
+	}
+	NewTeamComponent->SetTeam(OldTeamComponent->GetTeam());
 }
