7a39dc330db1533aaa6a48219de83298c2305f18
floweryclover <floweryclover@gmail.com>
2025-05-09 14:24:46 +0900

    코드수정: 무기 계열 클래스들에서 IsValid 검증 로직 추가

--- Source/ShootingStar/Public/Gun.cpp
+++ Source/ShootingStar/Public/Gun.cpp
@@ -67,50 +67,50 @@ void AGun::PlayReloadMontage()
 void AGun::StopReloadMontage()
 {
 }
-
-void AGun::PullTrigger()
-{
-    APlayerController* const OwnerController = Cast<APlayerController>(GetOwner());
-    if (!IsValid(OwnerController))
-    {
-        UE_LOG(LogTemp, Error, TEXT("OwnerController is nullptr!"));
-        return;
-    }
-    
-    APawn* const OwnerPawn = OwnerController->GetPawn();
-    if (!IsValid(OwnerPawn))
-    {
-        UE_LOG(LogTemp, Error, TEXT("OwnerPawn is nullptr!"));
-        return;
-    }
-
-    FVector Location = OwnerPawn->GetActorLocation() + OwnerPawn->GetActorForwardVector() * 100.0f;
-    FRotator Rotation = OwnerPawn->GetActorRotation();
-
-    FVector End = Location + Rotation.Vector() * MaxRange;
-
-    FHitResult Hit;
-    bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1);
-
-    if (bSuccess)
-    {
-        DrawDebugLine(GetWorld(), Location, End, FColor::Green, false, 2, 0, 2);
-        DrawDebugPoint(GetWorld(), Hit.Location, 10, FColor::Red, false, 2.0f);
-
-        FVector ShotDirection = -Rotation.Vector();
-        AActor* HitActor = Hit.GetActor();
-
-        if (HitActor)
-        {
-            if (!OwnerController)
-            {
-                UE_LOG(LogTemp, Error, TEXT("OwnerController is nullptr in TakeDamage!"));
-                return;
-            }
-
-            FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
-            HitActor->TakeDamage(Damage, DamageEvent, OwnerController, this);
-        }
-    }
-}
-
+//
+// void AGun::PullTrigger()
+// {
+//     APlayerController* const OwnerController = Cast<APlayerController>(GetOwner());
+//     if (!IsValid(OwnerController))
+//     {
+//         UE_LOG(LogTemp, Error, TEXT("OwnerController is nullptr!"));
+//         return;
+//     }
+//     
+//     APawn* const OwnerPawn = OwnerController->GetPawn();
+//     if (!IsValid(OwnerPawn))
+//     {
+//         UE_LOG(LogTemp, Error, TEXT("OwnerPawn is nullptr!"));
+//         return;
+//     }
+//
+//     FVector Location = OwnerPawn->GetActorLocation() + OwnerPawn->GetActorForwardVector() * 100.0f;
+//     FRotator Rotation = OwnerPawn->GetActorRotation();
+//
+//     FVector End = Location + Rotation.Vector() * MaxRange;
+//
+//     FHitResult Hit;
+//     bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1);
+//
+//     if (bSuccess)
+//     {
+//         DrawDebugLine(GetWorld(), Location, End, FColor::Green, false, 2, 0, 2);
+//         DrawDebugPoint(GetWorld(), Hit.Location, 10, FColor::Red, false, 2.0f);
+//
+//         FVector ShotDirection = -Rotation.Vector();
+//         AActor* HitActor = Hit.GetActor();
+//
+//         if (HitActor)
+//         {
+//             if (!OwnerController)
+//             {
+//                 UE_LOG(LogTemp, Error, TEXT("OwnerController is nullptr in TakeDamage!"));
+//                 return;
+//             }
+//
+//             FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
+//             HitActor->TakeDamage(Damage, DamageEvent, OwnerController, this);
+//         }
+//     }
+// }
+//
