07cbbb9bb1f9fe76e36b311bc685febdb395853a
floweryclover <floweryclover@gmail.com>
2025-05-14 20:43:32 +0900

    코드추가: FAIL_IF_NOT_SERVER() 매크로 함수 추가

--- Source/ShootingStar/ShootingStar.h
+++ Source/ShootingStar/ShootingStar.h
@@ -11,3 +11,14 @@ namespace CollisionChannels
 {
 	constexpr ECollisionChannel ResourceActor = ECC_GameTraceChannel1;
 }
+
+#define FAIL_IF_NOT_SERVER()																															\
+	if (!HasAuthority())																																\
+{																																					    \
+	UE_LOG(LogShootingStar,																															    \
+		Error,																																	        \
+		TEXT("서버에서만 호출되어야 하는 함수가 클라이언트에서 호출되었습니다. 파일: %hs, 함수: %hs, 라인: %d."), __FILE__, __FUNCTION__, __LINE__)     \
+	return;																																				\
+}																																					    \
+else																																					\
+	((void*)0)
