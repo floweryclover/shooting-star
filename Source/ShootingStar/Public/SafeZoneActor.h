5c86c369ebd6a7eec1e78f88b60857bfac57f9ec
floweryclover <floweryclover@gmail.com>
2025-05-14 19:34:47 +0900

    코드수정: SafeZoneActor의 Radius 관련 동작들을 기존 Scale 단위가 아니라 UU 단위로 동작하도록 수정

--- Source/ShootingStar/Public/SafeZoneActor.h
+++ Source/ShootingStar/Public/SafeZoneActor.h
@@ -14,22 +14,23 @@ class SHOOTINGSTAR_API ASafeZoneActor final : public AActor
 
 public:
     UFUNCTION(BlueprintCallable)
-    float GetRadius() const { return GetActorScale3D().X; };
+    float GetRadius() const { return GetActorScale3D().X * 50.0f; };
 
     UFUNCTION(BlueprintCallable)
     void SetRadiusByAlpha(const float Alpha01)
     {
         const float Radius = FMath::Lerp(InitialSafeRadius, FinalSafeRadius, Alpha01);
-        SetActorScale3D({Radius, Radius, Radius});    
+
+        SetActorScale3D({Radius / 50.0f, Radius / 50.0f, Radius / 50.0f });    
     }
     
 protected:
     virtual void BeginPlay() override;
     
-    // 자기장 크기 설정
+    // 자기장 크기 설정, uu 단위(언리얼 유닛 = cm, Location의 그 단위).
     UPROPERTY(EditAnywhere, Category = "SafeZone Settings")
-    float InitialSafeRadius = 150.f;
+    float InitialSafeRadius = 5000.0f;
     
     UPROPERTY(EditAnywhere, Category = "SafeZone Settings")
-    float FinalSafeRadius = 40.f;
+    float FinalSafeRadius = 1000.0f;
 };
